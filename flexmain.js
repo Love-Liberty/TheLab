3a9499f6ef8595fb1efd5c2a17f5bf03b10d3c06
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-12 15:43:00 +0100

    Create flexmain.js
    contains anything needed by the html file
    
    call to  listener on the [save] button

---
+++ flexmain.js
@@ -0,0 +1,6 @@
+import { setupNotesListeners } from './ui/listeners.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  setupNotesListeners(); // Attach listeners once DOM is ready
+});
+
