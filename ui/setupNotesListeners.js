25fb096e3c4b2d4114b1d8ac4d2406a1f985a9ff
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-14 08:09:09 +0100

    Update setupNotesListeners.js
    deleted
    
      console.log('📥 [save] params:', params);
    console.log('📥 [save] tags:', tags);
    console.log('📥 [save] tags type:', typeof tags, 'is array?', Array.isArray(tags));
    
    I don't think these are known at this point

--- ui/setupNotesListeners.js
+++ ui/setupNotesListeners.js
@@ -1,7 +1,5 @@
 import{ saveNoteWithTags }from '../db/saveNoteWithTags.js';
 console.log('setupNoteslisteners.js');
-console.log('🔍 [setup] userChoices:', userChoices);
-console.log('🔍 [setup] Array?', Array.isArray(userChoices));
 export function setupNotesListeners() {
   console.log('setupNotesListeners()');
   const saveButton = document.getElementById('save-notes');
