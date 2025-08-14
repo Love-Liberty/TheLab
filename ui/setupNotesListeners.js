0d659f70a870a17ac9f80d2db6c61f0709e88421
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-14 08:12:17 +0100

    Update setupNotesListeners.js
    added
    console.log 
    extra info

--- ui/setupNotesListeners.js
+++ ui/setupNotesListeners.js
@@ -1,13 +1,13 @@
 import{ saveNoteWithTags }from '../db/saveNoteWithTags.js';
 console.log('setupNoteslisteners.js');
 export function setupNotesListeners() {
-  console.log('setupNotesListeners()');
+  console.log('setupNotesListeners() on [save] button');
   const saveButton = document.getElementById('save-notes');
   if (!saveButton) {
     console.warn('❌ Error in event listener: save button not found');
     return;
   }
-
+  console.log('setupNotesListeners() -> saveNoteWithTags');
 saveButton.addEventListener('click', saveNoteWithTags);
 
 }
