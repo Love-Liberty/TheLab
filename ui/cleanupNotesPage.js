9735eae65749940dff830f6ed68a0ab5bf96568d
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-14 15:00:30 +0100

    Update cleanupNotesPage.js
    blank placeholder

--- ui/cleanupNotesPage.js
+++ ui/cleanupNotesPage.js
@@ -1,1 +1,12 @@
+//cleanupNotesPage.js
 
+console.log('cleanupNotesPage.js');
+
+export async function cleanupNotesPage (){
+console.log('cleanupNotesPage()');
+
+//renderNotes
+  //reset tags?
+  //reset notes text input  'Saved'? 
+  
+}
