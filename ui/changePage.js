1744397de1eaea8992001a7536fb5b0e91b170ee
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-15 13:02:55 +0100

    Update changePage.js
    added logic to respond to [older] [newer] button clicks
    
    then do match & check bounds >0  !>totalCount
    
    calls the updated 
    displayNotes(page, totalCount)

--- ui/changePage.js
+++ ui/changePage.js
@@ -1,3 +1,31 @@
 //changePage.js
 //response to click on the [older] or [Newer] page buttons on the rendered list of published notes. Calls for the next page of notes to be rendered
 console.log('changePage.js');
+
+
+// Updated changePage.js
+console.log('changePage.js');
+
+import { displayNotes } from './displayNotes.js';
+
+export function changePage(currentPage, totalCount, direction) {
+  console.log('changePage called:', { currentPage, totalCount, direction });
+  
+  const pageSize = 10;
+  const totalPages = Math.ceil(totalCount / pageSize);
+  let newPage = currentPage;
+  
+  if (direction === 'older') {
+    newPage = currentPage - 1;
+    if (newPage < 1) newPage = 1;
+  } else if (direction === 'newer') {
+    newPage = currentPage + 1;
+    if (newPage > totalPages) newPage = totalPages;
+  }
+  
+  // Only call displayNotes if page changed
+  if (newPage !== currentPage) {
+    console.log('Loading page:', newPage);
+    displayNotes(newPage, totalCount);
+  }
+}
