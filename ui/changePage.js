f8938ab2ef592869d247bf0ac4df96bc68e2649d
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-15 12:20:30 +0100

    Create changePage.js
    Respond to page buttons on the rendered list of notes

---
+++ ui/changePage.js
@@ -0,0 +1,3 @@
+//changePage.js
+//response to click on the [older] or [Newer] page buttons on the rendered list of published notes. Calls for the next page of notes to be rendered
+console.log('changePage.js');
