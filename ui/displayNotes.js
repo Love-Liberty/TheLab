b1181214b1b60e1eceb998f4ed288485e5356ca8
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-16 11:42:54 +0100

    Update displayNotes.js
    deleted older commented-out version

--- ui/displayNotes.js
+++ ui/displayNotes.js
@@ -34,33 +34,3 @@ export async function displayNotes(page = 1, totalCount = null) {
     }
   }
 }
-
-/* older functioning but no parameters
-
-import { createSupabaseClient } from '../db/client.js';
-import { fetchNotes } from "../db/notes.js";  
-import { renderNotes } from "../db/notes.js";  
-
-export async function displayNotes() {
-  console.log('displayNotes()');
-  const pageSize = 10;
-  try {
-    const supabase = createSupabaseClient();
-    const { notes, totalCount } = await fetchNotes(supabase, 1, pageSize);
-    console.log ('notes:',notes,'totalCount:', totalCount)
-    // Render the notes fetch return { notes: data, totalCount: count };
-    renderNotes(notes, totalCount, 1, pageSize);
-    console.log('displayNotes() fetch ended');
-    
-  } catch (error) {
-    console.error('Error displaying notes:', error);
-    const output = document.getElementById('output');
-    if (output) {
-      output.innerHTML = `
-        <div class="text-red-700 p-4">
-          <p>Failed to load notes. Please try again.</p>
-        </div>
-      `;
-    }
-  }
-} */
