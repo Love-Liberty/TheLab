6724ad2cc0ad9ef80cb2ae5b74048da29f1ede06
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-16 11:41:51 +0100

    Update reactToNoteClick.js
    added comments

--- ui/reactToNoteClick.js
+++ ui/reactToNoteClick.js
@@ -1,3 +1,9 @@
 //reactToNoteClick.js
 //response to click on a note in the rendered list of published notes
 console.log('handleNoteClick.js');
+
+
+//there is a write to db function in db/notes.js
+// saveNoteStatus(supabase, noteId, newStatus)    return { data, error };
+
+//where is update status logic that cycles?
