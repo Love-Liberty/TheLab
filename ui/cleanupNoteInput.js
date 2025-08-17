d7ef8294bc2c6c36831f9a70960938f9e0359bb7
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-17 20:37:04 +0100

    Update cleanupNoteInput.js
    added parameter
    (message)
    
    so can use function for just clearing or giving feedback of having saved

--- ui/cleanupNoteInput.js
+++ ui/cleanupNoteInput.js
@@ -2,13 +2,13 @@
 
 console.log('cleanupNoteInput.js');
 
-export async function cleanupNoteInput (){
+export async function cleanupNoteInput (message='Cleared'){
 console.log('cleanupNoteInput()');
 //  const noteContent = document.getElementById('note-content')
 
 const note = document.getElementById('note-content');
 note.value = "";
-note.placeholder = "Cleared";
+note.placeholder = message;
 
 setTimeout(() => {
   note.placeholder = "Enter your notes here & press [Save/send]... (Use the checkboxes to tag your note for later search & retrieval ) The saved notes can be seen by scrolling down. When you look at saved notes you can click them to mark them as pending, completed or abandonded.";
