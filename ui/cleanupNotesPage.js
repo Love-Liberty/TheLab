27dbb56637670de79e7542094d390ad8f98d634e
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-17 19:59:55 +0100

    Update cleanupNotesPage.js
    added
    clear note
    show place holder 'saved'
    then 2 seconds later show original placeholder
    
    (text is separate duplicate from notes.html)

--- ui/cleanupNotesPage.js
+++ ui/cleanupNotesPage.js
@@ -4,7 +4,18 @@ console.log('cleanupNotesPage.js');
 
 export async function cleanupNotesPage (){
 console.log('cleanupNotesPage()');
+//  const noteContent = document.getElementById('note-content')
 
+const note = document.getElementById('note-content');
+note.value = "";
+note.placeholder = "Saved";
+
+setTimeout(() => {
+  note.placeholder = ""Enter your notes here & press [Save/send]... (Use the checkboxes to tag your note for later search & retrieval ) The saved notes can be seen by scrolling down. When you look at saved notes you can click them to mark them as pending, completed or abandonded."";
+}, 2000); // 2 seconds later
+
+
+  
 //renderNotes
   //reset tags?
   //reset notes text input  'Saved'? 
