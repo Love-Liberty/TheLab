ba923caff9fc19e70c05f9978aec278c66c75283
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-17 20:05:09 +0100

    Update reactToClearAllButton.js
    added import 
    cleanupNotesInput.js
    
    calls 
    cleanupNotesInput()

--- ui/reactToClearAllButton.js
+++ ui/reactToClearAllButton.js
@@ -1,8 +1,9 @@
 //reactToClearAllButton.js
+import { cleanupNoteInput } from './cleanupNoteInput.js';
 
 console.log('reactToClearAllButton.js');
 
 export function reactToClearAllButton(){
 console.log('reactToClearAllButton()');
-  
+cleanupNoteInput();  
 }
