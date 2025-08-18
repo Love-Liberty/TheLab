d3089116d6f7ad5601f040012548cf2a4ec4115f
Love Liberty <48633589+Love-Liberty@users.noreply.github.com>
2025-08-18 16:11:58 +0100

    Update login.js
    added log

--- db/login.js
+++ db/login.js
@@ -1,8 +1,10 @@
+//login.js
 import { createSupabaseClient } from './client';
-
+console.log('login.js');
 const supabase = createSupabaseClient();
 
 async function signup() {
+  console.log('signup()');
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   const { error } = await supabase.auth.signUp({ email, password });
@@ -11,6 +13,7 @@ async function signup() {
 }
 
 async function login() {
+  console.log('login()');
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   const { error, session } = await supabase.auth.signInWithPassword({ email, password });
